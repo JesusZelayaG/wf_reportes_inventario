e9d1130dda43a177ed83b013aa99ade0732f81cc
Jesus <jesus.zelayag@gmail.com>
2019-11-02 14:52:18 -0600

    Modificaciones menores en algunos formularios
    Modificaciones en formularios

---
+++ wf_repo_inven/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wf_repo_inven.Models
+{
+    public class Account
+    {
+        public string Name { get; set;}
+        public string Password { get; set; }
+    }
+}
